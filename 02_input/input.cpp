d0f87bccd4b74b851395eb6a0eb0c15cc6493a2e
Jerry <jerry@Jerryui-MacBook.local>
2018-12-12 22:54:35 +0900

    Update

---
+++ 02_input/input.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    char text[10];
+    cout << "Please type your name ! : ";
+    cin >> text;
+    cout << "\nHello, " << text << "!" << endl;
+    return 0;
+}
